a9cde97d039c080947ecdd501c48cd20d5e67b7b
ByDefalt <rousvalromain@gmail.com>
2024-11-26 18:35:36 +0100

    Create BatimentDAOTest.java

---
+++ src/test/java/Defalt/Batiments/DAO/BatimentDAOTest.java
@@ -0,0 +1,21 @@
+package Defalt.Batiments.DAO;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BatimentDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void serializeBatiment() {
+    }
+
+    @Test
+    void deserializeBatiment() {
+    }
+}
